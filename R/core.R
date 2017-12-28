0793a9189506dd7e8a1e27eaf3c92e27b3264212
strnda <strnadf@fzp.czu.cz>
2017-12-28 14:12:55 +0100

    minor tweaks to the seRcter function added eveRloving function

--- R/core.R
+++ R/core.R
@@ -1,21 +1,28 @@
-library(ggplot2, quietly = T)
-library(data.table, quietly = T)
+lop <- c('ggplot2', 'data.table')
+to.instal <- lop[!(lop %in% row.names(installed.packages()))]
+
+if(length(to.instal) != 0) {install.packages(to.instal)}
+
+lapply(lop, library, character.only = T)
 
 #' ...
 #'
+#'Function revealing that you are actually Bruce Wayne using a secret identity... (function returns grob - graphical object)
+#'
 #' @param name Enter your name (as character string) to reveal your biggest secret...
 #'
-#' @return
-#' @export
+#' @return .
+#' @export .
 #'
-#' @examples
-secret <- function(name = 'Yannis') {
+#' @examples library(RFun)
+#' secRet('Bruce Wayne')
+secRet <- function(name = 'Yannis') {
 
   x <- seq(0, 2*pi, length = (15000))
   d0 <- data.table(x = 8*cos(x), y = 4*sin(x))
 
   x <- c(seq(3, 7, 0.001), seq(-7, -3, 0.001))
-  d1 <- data.table(x = x, y = c(3*sqrt(1-(x/7)^2), -(3*sqrt(1-(x/7)^2))))
+  d1 <- data.table(x = x, y = c(3*sqrt(1 - (x/7)^2), -(3*sqrt(1 - (x/7)^2))))
   d1 <- d1[d1$y > -3*sqrt(33)/7,]
 
   d1.1 <- d1[(d1$y > 0) & (d1$x > 0),]
@@ -35,7 +42,7 @@ secret <- function(name = 'Yannis') {
   d1.4 <- rbind(list(min(d1.4$x), 0), d1.4)
 
   x <- seq(-4, 4, 0.001)
-  d2 <- data.table(x = x, y = abs(x/2)-(3*sqrt(33)-7)*x^2/112-3+sqrt(1-(abs(abs(x)-2)-1)^2))
+  d2 <- data.table(x = x, y = abs(x/2) - (3*sqrt(33) - 7)*x^2/112 - 3 + sqrt(1 - (abs(abs(x) - 2) - 1)^2))
   d2 <- rbind(list(min(d2$x), 0), d2)
   d2 <- rbind(d2, list(max(d2$x), 0))
 
@@ -51,7 +58,7 @@ secret <- function(name = 'Yannis') {
   d3.2 <- rbind(list(min(d3.2$x), 0), d3.2)
 
   x <- c(seq(0.5, 0.75, 0.001), seq(-0.75, -0.5, 0.001))
-  d4 <- data.table(x = x, y = 3*abs(x)+0.75)
+  d4 <- data.table(x = x, y = 3*abs(x) + 0.75)
 
   d4.1 <- d4[d4$x > 0,]
   d4.1 <- rbind(list(min(d4.1$x), 0), d4.1)
@@ -67,7 +74,7 @@ secret <- function(name = 'Yannis') {
   d5 <- rbind(d5, list(max(d5$x), 0))
 
   x <- c(seq(-3, -1, 0.001), seq(1, 3, 0.001))
-  d6 <- data.table(x = x, y  = 6 * sqrt(10)/7+(1.5-0.5*abs(x))*sqrt(abs(abs(x)-1)/(abs(x)-1))-6*sqrt(10)*sqrt(4-(abs(x)-1)^2)/14)
+  d6 <- data.table(x = x, y  = 6 * sqrt(10)/7 + (1.5 - 0.5*abs(x))*sqrt(abs(abs(x) - 1)/(abs(x) - 1)) - 6*sqrt(10)*sqrt(4 - (abs(x) - 1)^2)/14)
 
   d6.1 <- d6[d6$x > 0,]
   d6.1[is.na(d6.1)] = 0
@@ -93,9 +100,63 @@ secret <- function(name = 'Yannis') {
     geom_polygon(data = d6.2, aes(x = x, y = y), color = 'black', fill = 'black') +
     ggtitle(paste(name,'is BATMAN ! ! !')) +
     theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5)) +
     coord_fixed()
 
   return(bat)
 }
 
-?secret
+#' Love is everywhere
+#'
+#' Let the (R) world know...
+#'
+#' @param love Character string containing your name and the name of your beloved
+#' @param size grid size (of your love)
+#' @param colour1 .
+#' @param colour2 .
+#' @param n.hearts number of hearts within the grid
+#'
+#' @return .
+#' @export .
+#'
+#' @examples library(RFun)
+#' eveRloving()
+#' eveRloving('Phill + Caroline', 200, 100, '#1f3b51', '#aac6dc')
+eveRloving <- function(love = NULL, size = 150, n.hearts = 50, colour1 = 'hotpink', colour2 = 'red4') {
+
+  dt <- data.table(numbers = seq(0, 2*pi, by = 0.01))
+
+  dt <- dt[, `:=` (x = 16*sin(numbers)^3,
+                   y = 13*cos(numbers) - 5*cos(2*numbers) - 2*cos(3*numbers) - cos(4*numbers))]
+
+  rotate <- function(dta, degree) {
+    ## reference
+    ## http://en.wikipedia.org/wiki/Rotation_matrix
+    mat <- as.matrix(dta)
+    theta <- degree/180*pi
+    r <- matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)), byrow = TRUE, ncol = 2)
+    mat <- (mat - colMeans(mat)) %*% r
+    return(mat)
+  }
+
+  hearts <- list()
+  for (i in 1:n.hearts) {
+    hearts[[i]] <- cbind(rotate(dt[,2:3]*sample(seq(.5,1.5,.01), 1), sample(-30:30, 1)),i)
+    for(j in 1:2) {
+      hearts[[i]][,j] <- hearts[[i]][,j] + sample(-size:size, 1)
+    }
+  }
+  gg_hearts <- data.table(do.call(rbind, hearts))
+  names(gg_hearts) <- c('x', 'y', 'id')
+
+  h <- ggplot(gg_hearts, aes(x = x, y = y, group = factor(id), color = id, fill = id)) +
+    geom_polygon(show.legend = FALSE, alpha = .75) +
+    scale_color_continuous(low = colour1, high = colour2) +
+    scale_fill_continuous(low = colour1, high = colour2) +
+    ggtitle(bquote(bold(.(love)))) +
+    coord_fixed() +
+    theme_void() +
+    theme(plot.title = element_text(hjust = 0.5))
+
+  return(h)
+}
